62e223cca5179d7f0f60238ecd71f3e165e7c75f
madmaxmehdi44 <madmaxmehdi44@gmail.com>
2025-08-09 03:32:28 +0330

    goh

---
+++ .history/next.config_20250809032848.ts
@@ -0,0 +1,23 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true
+};
+module.exports = nextConfig;
+
+import path from 'path';
+
+module.exports = {
+  webpack: (config: { resolve: { alias: { [x: string]: string; }; }; module: { rules: { test: RegExp; use: string; }[]; }; }, { isServer }: unknown) => {
+    // مثال: اضافه کردن alias
+    config.resolve.alias['@components'] = path.join(__dirname, 'components');
+
+    // مثال: اضافه کردن loader سفارشی
+    config.module.rules.push({
+      test: /\.md$/,
+      use: 'raw-loader',
+    });
+
+    return config;
+  },
+};
